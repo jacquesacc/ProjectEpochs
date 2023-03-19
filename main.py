cfcad00030c1cf8d780a46c0ecdf626873e3b2b0
jacques <jacques_acc@proton.me>
2023-03-19 20:35:11 +0100

    First push

---
+++ main.py
@@ -0,0 +1,39 @@
+import datetime as dt
+import streamlit as st
+
+project = ['Thena', 'Ramses', 'SolidLizard']
+first_flip = [dt.datetime(2023, 1, 12, 0, 0, 0, tzinfo=dt.timezone.utc), dt.datetime(2023, 3, 23, 0, 0, 0, tzinfo=dt.timezone.utc), dt.datetime(2023, 2, 2, 0, 0, 0, tzinfo=dt.timezone.utc)]
+
+st.title('Time to next epoch for Solidly forks')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Project")
+
+with col2:
+   st.header("Next epoch")
+
+with col3:
+   st.header("Time to epoch")
+
+for i in range(0,len(project)):
+    epoch = 0
+    next_epoch = first_flip[i]
+    if next_epoch < dt.datetime.now().astimezone(dt.timezone.utc):
+        while next_epoch < dt.datetime.now().astimezone(dt.timezone.utc):
+            epoch = epoch + 1
+            next_epoch = next_epoch + dt.timedelta(days=7)
+
+        time_to_epoch = next_epoch - dt.datetime(dt.datetime.now().year, dt.datetime.now().month, dt.datetime.now().day, dt.datetime.now().hour, dt.datetime.now().minute, dt.datetime.now().second).astimezone(dt.timezone.utc)
+
+    with col1:
+        st.write(project[i])
+
+    with col2:
+        st.write(epoch)
+
+    with col3:
+        st.write(time_to_epoch)
+
+
